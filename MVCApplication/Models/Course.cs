a769088ee27fceba98fcebbed12c28a2ed20154d
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:08:03 +0000

    Setting git up on project

---
+++ MVCApplication/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCApplication.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        // Name must be included.
+        [Required]
+        public string Name  { get; set; }
+        // Description has a max of 100 chars.
+        [StringLength(100)]
+        public string Description{ get; set; }
+        // Customised label to Online Course
+        [Display(Name="Online Course")]
+        public bool IsVirtual { get; set; }
+    }
+}
---
+++ TutorialAspNet/WebFormApplication/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebFormApplication
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Get when the application started.
+            Application["ApplicationStartDateTime"] = DateTime.Now;
+        }
+
+        // Stores session specific information. (Same browser at a limited amount of time.)
+        void Session_Start(object sender, EventArgs e)
+        {
+            // Get when the session started. 
+            Session["SessionStartDateTime"] = DateTime.Now;
+        }
+    }
+}
