406595fe17646ae94465eb90558a3fe6c1da2c25
Jason Hall <jasonahall1994@gmail.com>
2019-11-27 15:01:34 +0000

    continue tutorial chapter 6

--- MVCApplication/Controllers/CourseController.cs
+++ MVCApplication/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
-﻿using MVCApplication.Models;
+﻿using MVCApplication.DataAccess;
+using MVCApplication.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,30 @@ namespace MVCApplication.Controllers
 {
     public class CourseController : Controller
     {
+        private RouxAcademyDbContext db = new RouxAcademyDbContext();
+
         // GET: Course
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Online()
+        {
+            // LINQ's method based syntax
+            //var courses = db.Courses.Where(c => c.IsVirtual)
+            //                        .OrderBy(c => c.Name)
+            //                        .ToList();
+            //
+            // return View(courses);
+
+            // LINQ's declarative query syntax
+            var courses = from c in db.Courses
+                          where c.IsVirtual
+                          orderby c.Name
+                          select c;
+
+            return View(courses.ToList());
         }
 
         // GET: Course/Details/5
@@ -36,6 +57,12 @@ namespace MVCApplication.Controllers
                 if (ModelState.IsValid)
                 {
                     // TODO: Add insert logic here
+
+                    using (var context = new RouxAcademyDbContext())
+                    {
+                        context.Courses.Add(course);
+                        context.SaveChanges();
+                    }
 
                     return RedirectToAction("Index");
                 }
